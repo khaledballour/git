25e8f65c3d6a0f8eb64cd2c583c61279f5c07db8
= <=>
2018-12-26 14:37:52 +0200

    First commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -17,8 +17,8 @@ public class Git {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("hello");
-        
+        System.out.println("hello2");
+        git
             
         
     }
